3dc19936456f1773930f8494885ea014f8c85c07
freelancerjolil <freelancerjolil@gmail.com>
2024-08-20 15:46:10 +0600

    Setup Complete for Biker Zone

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+<script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+            'font-poppins': "Poppins", "sans-serif";
+          }
+        }
+      }
+    }
+  </script>
